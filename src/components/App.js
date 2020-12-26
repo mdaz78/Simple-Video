10a0f6183c1c42593264a87a3cb2811148b6dafe
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-12-27 02:28:06 +0530

    feat: Convert App.js to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,64 +1,51 @@
-import React, { Component } from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
-import VideoList from '../components/VideoList';
-import VideoDetail from './VideoDetail';
-import Loader from './Loader';
+import React, { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "../components/VideoList";
+import VideoDetail from "./VideoDetail";
+import Loader from "./Loader";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    this.state = {
-      videos: [],
-      selectedVideo: null,
-      loading: true,
-    };
-  }
-
-  componentDidMount() {
-    this.onTermSubmit('mac');
-  }
-
-  onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
+  const onVideoSelect = (video) => setSelectedVideo(video);
+  const onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
 
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-      loading: false,
-    });
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
+    setLoading(false);
   };
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
-  };
+  useEffect(() => {
+    onTermSubmit("react js");
+  }, []);
 
-  render() {
-    const { videos, selectedVideo, loading } = this.state;
+  if (loading) {
+    return <Loader />;
+  }
 
-    if (loading) {
-      return <Loader />;
-    }
-
-    return (
-      <div className='ui container m-top-3'>
-        <SearchBar onTermSubmit={this.onTermSubmit} />
-        <div className='ui grid'>
-          <div className='ui row'>
-            <div className='eleven wide column'>
-              <VideoDetail video={selectedVideo} />
-            </div>
-            <div className='five wide column'>
-              <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
-            </div>
+  return (
+    <div className="ui container m-top-3">
+      <SearchBar onTermSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList videos={videos} onVideoSelect={onVideoSelect} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default App;
